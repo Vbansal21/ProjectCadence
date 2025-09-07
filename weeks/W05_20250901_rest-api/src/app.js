f2217b410bc7b84e30d8c81b423014e587ac0358
Vbansal21 <vbansal2105@gmail.com>
2025-09-07 13:32:53 +0000

    feat: simple frontend index.html to interact - setted up

--- weeks/W05_20250901_rest-api/src/app.js
+++ weeks/W05_20250901_rest-api/src/app.js
@@ -13,19 +13,37 @@ import articlesRouter from "./routes/articles.routes.js";
 import commentsRouter from "./routes/comments.routes.js";
 import { requestId } from "./middleware/requestId.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
 app.set("trust proxy", 1);
 
-app.use(helmet());
+// app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      useDefaults: true,
+      directives: {
+        "script-src": ["'self'", "'unsafe-inline'"],
+        "img-src": ["'self'"]
+      }
+    }
+    // contentSecurityPolicy: false
+  })
+);
 app.use(cors(config.cors));
 app.use(rateLimit(config.rate));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 morgan.token("rid", (req) => req.id);
 app.use(requestId);
 app.use(morgan(':method :url :status - rid=:rid - :response-time ms'));
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 // Root index (simple API metadata)
 app.get("/", (_req, res) => {
